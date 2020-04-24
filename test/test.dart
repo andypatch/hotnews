cf60232faf4161139821d52ab55061de85d912c1
andrea_montagner <patch.montagner@gmail.com>
2020-04-24 17:13:35 +0200

    ok

---
+++ test/test.dart
@@ -0,0 +1,11 @@
+import 'package:test/test.dart';
+import '../lib/counter.dart';
+
+void main() {
+  test('Counter value should be incremented',() {
+    final counter = Counter();
+    counter.increment();
+
+    expect(counter.value, 1);
+  });
+}
